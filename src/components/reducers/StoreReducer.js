319845b0e7643480bf44157b0152ca04c1e9f9ac
Alex Wong <fi0088@wayne.edu>
2019-04-04 18:49:22 -0700

    Appended final cake items

--- src/components/reducers/StoreReducer.js
+++ src/components/reducers/StoreReducer.js
@@ -4,6 +4,8 @@ import cake3 from '../images/strawberry-roll.jpg';
 import cake4 from '../images/plum-streusel.jpg';
 import cake5 from '../images/berry-sweet.jpg';
 import cake6 from '../images/chocolate-cappuccino.jpg';
+import cake7 from '../images/color-palette.jpg';
+import cake8 from '../images/date-cake.jpg';
 
 import cupcake1 from '../images/strawberry-tart.jpg';
 import cupcake2 from '../images/sweet-chocolate.jpg';
@@ -20,6 +22,8 @@ const initState = {
         {id:4, title: "Plum Streusel", desc: "See it finished in your mind.", img: cake4},
         {id:5, title: "Berry Sweet", desc: "Decide where your cloud lives.", img: cake5},
         {id:6, title: "Chocolate Cappuccino", desc: "Take a step back and look.", img: cake6},
+        {id:7, title: "Color Palette", desc: "It's all a game of angles.", img: cake7},
+        {id:8, title: "Date Cake", desc: "Put your feelings into it.", img: cake8},
     ],
     cupcake: [
         {id:1, title: "Strawberry Tart", desc: "Always one step further.", img: cupcake1},
@@ -27,7 +31,7 @@ const initState = {
         {id:3, title: "Sweet Frosting", desc: "Something you can't fail at.", img: cupcake3},
         {id:4, title: "Royal Red", desc: "Wherever you want to be.", img: cupcake4},
         {id:5, title: "Floral Pastel", desc: "We have plenty of time.", img: cupcake5},
-        {id:6, title: "Party Muffin", desc: "bring them into your world.", img: cupcake6},
+        {id:6, title: "Party Muffin", desc: "Bring them into your world.", img: cupcake6},
     ]
 }
 
