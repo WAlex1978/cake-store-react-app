d371c5c06f9799a4aa6affaf6dcbf9d600e234f1
Alex Wong <fi0088@wayne.edu>
2019-04-06 12:37:31 -0700

    Checkout button

--- src/components/cart/CartFooter.js
+++ src/components/cart/CartFooter.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'shards-react';
+import React, { Component, Fragment } from 'react';
+import { Button, Row, Col } from 'shards-react';
 import { connect } from 'react-redux';
 import Styled from '../../styled-components/Styled';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faLock)
 
 const mapStateToProps = (state) => {
     return {
@@ -12,13 +18,25 @@ const mapStateToProps = (state) => {
 class CartFooter extends Component {
     render() { 
         return (
-            <Styled.Text size="1.4rem" align="right">
-                <Row>
-                    <Col xs={8} />
-                    <Col xs={2}>Total:</Col>
-                    <Col xs={2}>${this.props.total.toFixed(2)}</Col>
-                </Row>
-            </Styled.Text>
+            <Fragment>
+                <Styled.Text size="1.4rem">
+                    <Row>
+                        <Col xs={9} />
+                        <Col xs={1}>Total:</Col>
+                        <Col xs={2} style={{textAlign: "right"}}>${this.props.total.toFixed(2)}</Col>
+                    </Row>
+                </Styled.Text>
+                    <Row>
+                        <Col xs={9} />
+                        <Col xs={3} style={{marginTop: "30px"}}>
+                            <Button block squared theme="success">
+                                <FontAwesomeIcon icon={faLock} style={{marginRight: "5px"}}/>
+                                Checkout
+                            </Button>
+                        </Col>
+                    </Row>
+            </Fragment>
+
         );
     }
 }
